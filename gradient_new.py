0053f878423178f4924e65f9d0b968b6b3169430
Steven <bishopsscope@csu.fullerton.edu>
2022-09-20 15:04:33 -0700

    Made a few updates to gradient_new.py. The primary one being that it now self-adjusts alpha and I introduces an increase/decrease fluctuation rate percentage to fit the data more accurately

--- gradient_new.py
+++ gradient_new.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
 
+np.set_printoptions(suppress = True)
+
 # Can use numpy for matrix multiplication
 
 def Residual_Sum(w, x, y, num_examples, getMean = False):
     sum = 0
 
     if not getMean:
+        
         for i in range(num_examples):
             predict_val = w[0]
             predict_val += np.dot(w[1:], x[i])
@@ -35,10 +38,13 @@ def R_Sq(w, x, y, num_examples):
        sum += (y[i, 0] - Mean) ** 2
     
     return 1 - (RSS/sum)
-    
 
 # Can be adjusted as needed
-alpha = 0.008 #0.008
+alpha = 0.005 # 0.00000246505314 # 0.007 #0.008
+
+inc_fluct_rate = 0.5 # 1.2
+
+dec_fluct_rate = 0.5 # 0.14
 
 classifier = "Y"
 
@@ -124,9 +130,15 @@ def get_predict_val(w, x):
 # y - A list containing the expected output value
 # alpha - The learning rate
 # num_examples - The number of rows in x
-def train_data(w, x, y, alpha, num_examples):
+def train_data(w, gradient, x, y, alpha, num_examples):
 
     N = len(w)
+
+    # This is the new gradient / old gradient summed
+    g_change = 0
+
+    new_g = 0
+    old_g = 0
 
     #v = []
     v = np.array([])
@@ -153,24 +165,60 @@ def train_data(w, x, y, alpha, num_examples):
 
             predict_val += np.dot(w[1:], x[i])
 
+            # Gradient
             sum += (predict_val - y[i, 0]) * x_val
+            
 
-            #print(sum)
+        if gradient[j] != None:
+
+            # Store the new and old gradient values
+            new_g = abs(sum)
+            old_g = abs(gradient[j])
+
+            # Update the sum of all gradients, g_change
+            if new_g > old_g:
+
+                g_change += (new_g % old_g) / old_g
+
+            elif new_g < old_g:
+
+                g_change -= (new_g / old_g)
+        
+            #new_g += sum
+            #old_g += gradient[j]
+
+        # Update the old gradient
+        gradient[j] = sum
+
+        # At this point, we have a gradient calculated
 
         #print("The gradient is: " + str(sum))
 
         sum *= (alpha / num_examples)
 
 
-        #print("v before: " + str(v))
         v = np.append(v, [w[j] - sum])
-        #print("v after: " + str(v))
 
         #w[j] -= sum
 
-    #print("V = " + str(v))
+    # Check if we're overshooting on average
+    if g_change > 0:
 
-    return v
+        print("The gradient change is positive (" + str(g_change) + ")! Reducing alpha")
+        alpha = (alpha * dec_fluct_rate) * (1 - (min(1, g_change) / N))
+
+    elif g_change < 0:
+
+        print("The gradient change is negative(" + str(g_change) + ")! Increasing alpha")
+        alpha = alpha * (1 + (((min(1, abs(g_change)) / N)) * inc_fluct_rate))
+
+    #if old_g != 0:
+
+    #    g_change += abs(new_g) / abs(old_g)
+
+    #    alpha = (alpha * fluct_rate) * (1 - ((1 / N) * g_change))
+
+    return v, alpha
             
 
 # Transform pandas df to numpy array
@@ -188,14 +236,19 @@ orig_col_names = list(df.columns[:-1])
 
 w = np.array([1] * (len(orig_col_names) + 1))
 
+#w = np.array([611.18305705,12485.69095535,-453.59327085,-1.74367274,-9023.79250752])
+
+#w = np.array([67.956433, 0.03722, -0.004231477, -5.58388, -0.0591527])
+
+
 # Take first sample (training)
-#df_new = df.sample(frac = 0.75, replace=True)
+df_new = df.sample(frac = 0.75, replace=True)
 
 # Remove classifier
-training_x = df.loc[:, df.columns != classifier]
+training_x = df_new.loc[:, df.columns != classifier]
 
 # Remove features
-training_y = df.loc[:, df.columns == classifier]
+training_y = df_new.loc[:, df.columns == classifier]
 
 # Take second sample (testing)
 df_new = df.sample(frac = 0.25, replace=True)
@@ -227,18 +280,45 @@ print(training_y)
 
 print("w: " + str(w))
 
-for i in range(1500):
+print("Alpha: " + str(alpha))
+
+gradient = [None] * len(w)
+
+for i in range(1000):
 
     # Convert training_x and training_y to numpy
     training_x_numpy = training_x.to_numpy()    
     training_y_numpy = training_y.to_numpy()
 
-    #print(training_x_numpy[1])
-
-    w = train_data(w, training_x_numpy, training_y_numpy, alpha, len(training_x_numpy))
+    w, alpha = train_data(w, gradient, training_x_numpy, training_y_numpy, alpha, len(training_x_numpy))
 
     print("w: " + str(w))
 
     print("RMSE: " + str(RMSE(w, training_x_numpy, training_y_numpy, len(training_x_numpy))))
 
     print("R^2: " + str(R_Sq(w, training_x_numpy, training_y_numpy, len(training_x_numpy))))
+
+    print("Alpha: " + str(alpha))
+
+    print("Gradient: " + str(gradient))
+
+
+    # prev gradent
+    # g(w[0]) = 100 -> 105 -> +5%
+    # g(w[1]) = 100 -> 80 -> -20%
+    # g(w[2]) = 100 -> -80 -> -20%
+
+    # avg(g) = -35 / 3 = -11.66%
+
+    # (0.1 / 0.5)(1 - (1/3) * (35 / 45))
+
+    # new gradient
+    # g(w[0]) = 100 -> 75 -> -25%
+    # g(w[1]) = 100 -> 120 -> +20%
+    # g(w[2]) = 100 -> -60 -> -40%
+
+    # avg(g) = -45 / 3 = -15%
+
+    # diff(g) = -15 + 11.66 = -3.34%
+
+    # alpha = alpha * (1 + 0.5 * -3.34) = alpha * -1.67%
